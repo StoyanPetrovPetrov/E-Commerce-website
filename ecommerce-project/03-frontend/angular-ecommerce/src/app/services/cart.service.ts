ae2ad37bcf10912c5e19280f2cf010d828e3717a
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-06-06 18:50:21 +0300

    Update cart.service.ts

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -13,7 +13,19 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  //storage: Storage = sessionStorage;
+  storage: Storage = localStorage;
+
+
+  constructor() {
+    let data = JSON.parse(this.storage.getItem('cartItems')!);
+
+    if(data != null){
+      this.cartItems = data;
+
+      this.computeCartTotals();
+    }
+   }
 
   addToCart(theCartItem: CartItem) {
 
@@ -59,6 +71,13 @@ export class CartService {
 
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    //persist cart data
+    this.persistCartItems();
+  }
+
+  persistCartItems(){
+    this.storage.setItem('cartItems',JSON.stringify(this.cartItems));
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
