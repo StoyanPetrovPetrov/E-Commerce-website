2ec005c789309d17f6a50b3f147aaabbb2751d37
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-06-10 18:02:23 +0300

    Add files via upload

---
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/stoyan/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.stoyan.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //protect end point /api/orders
+
+        System.out.println("reached 1");
+
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/api/orders/**")
+                                .authenticated()
+                                .anyRequest().permitAll())
+                .oauth2ResourceServer()
+                .jwt();
+
+        http.cors();
+
+        //add content negotiation strategy
+
+        //http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+
+        //force a non-empty response body for 401's to make the response user friendly
+
+        //Okta.configureResourceServer401ResponseBody(http);
+
+        //System.out.println("reached 2");
+
+        return http.build();
+    }
+}
