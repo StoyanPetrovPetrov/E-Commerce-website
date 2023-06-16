e310701a361c44dc622e7586f7e9d383612fe444
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-06-16 18:33:19 +0300

    Update CheckoutService.java

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/stoyan/ecommerce/service/CheckoutService.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/stoyan/ecommerce/service/CheckoutService.java
@@ -1,8 +1,12 @@
 package com.stoyan.ecommerce.service;
 
+import com.stoyan.ecommerce.dto.PaymentInfo;
 import com.stoyan.ecommerce.dto.Purchase;
 import com.stoyan.ecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
     PurchaseResponse placeOrder(Purchase purchase);
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
