7bc9ced585cf1e46186a42e878b9bec556605263
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-06-16 18:34:35 +0300

    Update CheckoutController.java

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/stoyan/ecommerce/controller/CheckoutController.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/stoyan/ecommerce/controller/CheckoutController.java
@@ -1,8 +1,13 @@
 package com.stoyan.ecommerce.controller;
 
+import com.stoyan.ecommerce.dto.PaymentInfo;
 import com.stoyan.ecommerce.dto.Purchase;
 import com.stoyan.ecommerce.dto.PurchaseResponse;
 import com.stoyan.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -22,6 +27,15 @@ public class CheckoutController {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String>createPaymentIntent(@RequestBody PaymentInfo paymentInfo)throws StripeException{
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+
     }
 
 }
